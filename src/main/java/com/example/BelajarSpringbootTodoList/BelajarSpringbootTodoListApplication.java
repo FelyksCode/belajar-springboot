02bb9cd51af2720a12599b87bc0d3725ac279aa0
Felyks <gamingfelix78@gmail.com>
2025-02-11 16:12:45 +0700

    initial commit

---
+++ src/main/java/com/example/BelajarSpringbootTodoList/BelajarSpringbootTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.BelajarSpringbootTodoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringbootTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringbootTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BelajarSpringbootTodoList/BelajarSpringbootTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BelajarSpringbootTodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringbootTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
